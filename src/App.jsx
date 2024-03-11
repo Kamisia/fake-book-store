d80e66e94395bbd0c3ca461948f67a3889c77b6d
Kamisia <kamila.samczuk@gmail.com>
2024-03-11 13:29:25 +0100

    connection to Google Books API using Axios and React Query

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 function App() {
-  return <>Fake Book Store</>;
+  const fetchBooks = async () => {
+    const response = await axios.get(
+      "https://www.googleapis.com/books/v1/volumes?q=harry+potter"
+    );
+
+    return response.data;
+  };
+
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["books"],
+    queryFn: fetchBooks,
+  });
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error fetching data...</div>;
+  }
+
+  return (
+    <div>
+      <h1>Fake Book Store</h1>
+      {data.items.map((book) => (
+        <div key={book.id}>
+          <h2>{book.volumeInfo.title}</h2>
+          <p>{book.volumeInfo.authors.join(", ")}</p>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import { createRoot } from "react-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
