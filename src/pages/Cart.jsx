d34726c797a286a18205af0780f75f78f520ef67
Kamisia <kamila.samczuk@gmail.com>
2024-03-13 14:14:57 +0100

    modified:   src/pages/Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,18 +1,5 @@
-import { useSelector } from "react-redux";
-import { selectItems } from "../reducers/cartReducer";
 const Cart = () => {
-  const items = useSelector(selectItems);
-
-  return (
-    <div>
-      Cart
-      <ul>
-        {data.map((item) => (
-          <li key={items.id}>{item.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+  return <p>Cart</p>;
 };
 
 export default Cart;
