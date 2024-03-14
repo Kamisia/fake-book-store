0fcc384bc06333d7b95c632761691161c198209d
Kamisia <kamila.samczuk@gmail.com>
2024-03-14 14:31:36 +0100

    modified:   src/index.css 	modified:   src/pages/Cart.jsx

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import {
   selectItems,
@@ -11,23 +10,26 @@ const Cart = () => {
 
   const handleDelete = (itemId) => {
     dispatch(deleteItem(itemId));
-    console.log("klik");
   };
   const handleDeleteAll = () => {
     dispatch(deleteAllItems);
   };
   return (
     <div className="cart-container">
-      <h1>Cart</h1>
-
-      <div className="items-container">
+      <div className="container">
         {items &&
           items.map((item) => (
-            <div className="single-item" key={item.id}>
-              <h1>{item.title}</h1>
-              {item.authors && <p> ~{item.authors.join(", ")}</p>}
-              <img src={item.image} />
-              <button onClick={() => handleDelete(item.id)}>delete</button>
+            <div className="single-book" key={item.id}>
+              <div className="image-container-book">
+                <img src={item.image} />
+              </div>
+              <div className="content-container">
+                <h1>{item.title}</h1>
+                {item.authors && <p> ~{item.authors.join(", ")}</p>}
+              </div>
+              <div className="button-container">
+                <button onClick={() => handleDelete(item.id)}>delete</button>
+              </div>
             </div>
           ))}
 
