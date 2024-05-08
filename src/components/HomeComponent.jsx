37643a56affd1a3d038ecb18a0b3802a16f14227
Kamisia <kamila.samczuk@gmail.com>
2024-05-08 12:04:25 +0200

    Edited text

--- src/components/HomeComponent.jsx
+++ src/components/HomeComponent.jsx
@@ -5,15 +5,17 @@ const HomeComponent = () => {
       <div className="hero-content">
         <h1>FakeBookStore</h1>
         <p>
-          Welcome to Fake Book Store, where literature becomes your
-          indispensable companion on the journey through the world of
-          imagination. We offer a wide selection of books from various genres
-          and for all ages, ensuring unforgettable reading experiences. Our
-          platform enables easy searching for favorite titles and quick, secure
-          online purchases. Thanks to collaboration with renowned publishers, we
-          guarantee the highest quality of offered publications. Join our
-          community of readers and discover new literary worlds waiting to be
-          explored!
+          Welcome to Fake Book Store, where literature becomes
+          your&nbsp;indispensable companion on&nbsp;the&nbsp;journey through
+          the&nbsp;world of&nbsp;imagination. We offer a&nbsp;wide selection
+          of&nbsp;books from various genres and&nbsp;for&nbsp;all ages, ensuring
+          unforgettable reading experiences. Our&nbsp;platform enables easy
+          searching for&nbsp;favorite titles and&nbsp;quick, secure online
+          purchases. Thanks to&nbsp;collaboration with&nbsp;renowned publishers,
+          we&nbsp;guarantee the&nbsp;highest quality of&nbsp;offered
+          publications. Join our&nbsp;community of&nbsp;readers
+          and&nbsp;discover new literary worlds waiting
+          to&nbsp;be&nbsp;explored!
         </p>
       </div>
       <div
