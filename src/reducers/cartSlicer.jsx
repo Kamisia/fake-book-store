5074d55f1651ea3d0a9edd1ddc30cdbb4bc9cbc3
Kamisia <kamila.samczuk@gmail.com>
2024-05-13 09:58:31 +0200

    added saving items in the cart in localstorage

--- src/reducers/cartSlicer.jsx
+++ src/reducers/cartSlicer.jsx
@@ -1,36 +1,45 @@
 import { createSlice } from "@reduxjs/toolkit";
-const saveItemsToLocalStorage = (ietms) => {
+const saveItemsToLocalStorage = (items) => {
   localStorage.setItem("cart", JSON.stringify(items));
+};
+const getCartFromLocalStorage = () => {
+  const cart = localStorage.getItem("cart");
+  return cart ? JSON.parse(cart) : [];
 };
 export const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    items: [],
+    items: getCartFromLocalStorage() || [],
     searchResults: [],
   },
 
   reducers: {
     setItems: (state, action) => {
       state.items = action.payload;
+      saveItemsToLocalStorage(action.payload);
     },
     deleteItem: (state, action) => {
       const itemId = action.payload;
 
       state.items = state.items.filter((item) => item.id !== itemId);
+      saveItemsToLocalStorage(state.items);
     },
     deleteAllItems: (state) => {
       state.items = [];
+      saveItemsToLocalStorage([]);
     },
     addItem: (state, action) => {
       const newItem = action.payload;
 
       state.items.push(newItem);
+      saveItemsToLocalStorage(state.items);
     },
     updateItemQuantity: (state, action) => {
       const { id, quantity } = action.payload;
       const itemToUpdate = state.items.find((item) => item.id === id);
       if (itemToUpdate) {
         itemToUpdate.quantity = quantity;
+        saveItemsToLocalStorage(state.items);
       }
     },
     addSearchResults: (state, action) => {
